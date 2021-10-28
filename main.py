7eec4c6cadcd740a00a468bb8d527636202709f9
Prashant K. Sharma <prashaantsharmaa@gmail.com>
2021-10-28 17:27:30 +0530

    Inference Code Format

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def main():
             make_prediction(eval_examples, y_true, y_pred, args.output_dir)
        
         logger.info("\n%s", report)
-        output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
+        output_eval_file = os.path.join(args.output_dir, "eval_results_inference.txt")
         with open(output_eval_file, "w") as writer:
             logger.info("***** Writing results to file *****")
             writer.write(report)
